cda213cd49dc061c6e24e9d59fe27cc2368fa300
Kayla Webb <kaylarwebb@gmail.com>
2018-01-03 09:55:02 -0500

    Include dynamic date conversion and user location string to GET request

--- routers/concerts-router.js
+++ routers/concerts-router.js
@@ -7,11 +7,34 @@ const ajax = require('ajax-request');
 
 const { DATABASE, PORT } = require('../config');
 
+// //getConcertDates() {
+//   //get today's date
+//   const todaysDate = new Date();
+//   //remove extra seconds from the ISO date format
+//   const startDateTime = todaysDate.toISOString().slice(0,19)+'Z';
+//   //get tomorrow's date in seconds
+//   const tomorrowSeconds = todaysDate.setDate(todaysDate.getDate()+1);
+//   //convert tomorrow's seconds into a new Date object
+//   const tomorrowsDate = new Date(tomorrowSeconds);
+//   //convert tomorrow's date to an ISO string with extra seconds removed
+//   const endDateTime = tomorrowsDate.toISOString().slice(0,19)+'Z';
+  
+//   return {startDateTime, endDateTime};
+
+// }
+
 /* ======GET/READ REQUESTS ======= */
 
 router.get('/concerts', (req, res) => {
-  console.log(req);
-  fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=atlanta&startDateTime=2018-01-03T11:39:00Z&endDateTime=2018-01-04T11:39:00Z&apikey=${process.env.TKM_KEY}`)
+  //use req.query
+  console.log(req.query)
+  console.log(req.query.city)
+  const todaysDate = new Date();
+  const startDateTime = todaysDate.toISOString().slice(0, 19)+'Z';
+  const tomorrow = new Date(todaysDate.setDate(todaysDate.getDate()+2));
+  const endDateTime = tomorrow.toISOString().slice(0,11)+'00:00:00Z';
+
+  fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&classificationName=music&city=${req.query.city}&startDateTime=${startDateTime}&endDateTime=${endDateTime}&apikey=${process.env.TKM_KEY}`)
     .then(res => {
       return res.json(res);
     })
